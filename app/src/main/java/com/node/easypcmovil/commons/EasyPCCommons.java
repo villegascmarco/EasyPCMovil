87ff9dfee25e2ceaeb5ca9559653727b3df93fc6
Victor Santillan <victor.santillan.martinez.a@gmail.com>
2020-04-07 19:28:05 -0500

    Commit de merge

--- app/src/main/java/com/node/easypcmovil/commons/EasyPCCommons.java
+++ app/src/main/java/com/node/easypcmovil/commons/EasyPCCommons.java
@@ -2,7 +2,8 @@ package com.node.easypcmovil.commons;
 
 public class EasyPCCommons {
 
-    public static final String URL_SERVER = "http://192.168.0.15:8084/EasyPC/api/";
+    public static final String URL_SERVER = "http://192.168.1.70:8080/EasyPC/api/";
+    //public static final String URL_SERVER = "http://192.168.0.15:8084/EasyPC/api/";
 
     public static final String URL_USUARIO = URL_SERVER + "usuario/";
 }
